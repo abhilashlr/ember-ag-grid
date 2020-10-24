85c70eed4f92c8b1226f8b4a2097182de8a107fa
Mohit Sud <contact@mohitsud.com>
2020-10-24 10:29:40 -0700

    added support for vanilla js components

--- addon/components/ag-grid.js
+++ addon/components/ag-grid.js
@@ -5,13 +5,13 @@ export default class AgGridComponent extends Component {
 	gridOptions = {
 		columnDefs: this.args.columnDefs,
 		rowData: this.args.rowData,
+		components: this.args.components,
 		...this.args.additionalParameters
 	};
 
 	@action
 	async initializeGrid(elem) {
 		let { Grid } = await import('ag-grid-community');
-
 		new Grid(elem, this.gridOptions);
 	}
 }
